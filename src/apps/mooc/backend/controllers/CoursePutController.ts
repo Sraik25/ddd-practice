12b33366e3a6897a1d256908e4249e63f96d1c40
Olek MP <olek.mp23@gmail.com>
2022-01-10 16:56:36 -0500

    init: injection fileCourseRepository

--- src/apps/mooc/backend/controllers/CoursePutController.ts
+++ src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 import { Controller } from './Controller';
 
 export class CoursePutController implements Controller {
+  constructor(private courseCreator: CourseCreator) {}
+
   async run(req: Request, res: Response): Promise<void> {
-    // throw new Error('Method not implemented.');
+    const { id, name, duration } = req.body;
+
+    await this.courseCreator.run(id, name, duration);
+
     res.status(httpStatus.CREATED).send();
   }
 }
