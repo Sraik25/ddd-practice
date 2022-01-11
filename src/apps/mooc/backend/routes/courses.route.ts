6a53e7f10708097b23e49e5f9db25de18592f51b
Olek MP <olek.mp23@gmail.com>
2022-01-10 19:25:30 -0500

    Add: req validator

--- src/apps/mooc/backend/routes/courses.route.ts
+++ src/apps/mooc/backend/routes/courses.route.ts
@@ -1,8 +1,16 @@
 import { Response, Request, Router } from 'express';
+import { body } from 'express-validator';
+import { validateReqSchema } from '.';
 import CoursePutController from '../controllers/CoursePutController';
 import container from '../dependency-injection';
 
 export const register = (router: Router) => {
+  const reqSchema = [
+    body('id').exists().isString().optional(),
+    body('name').exists().isString(),
+    body('duration').exists().isString()
+  ];
+
   const controller: CoursePutController = container.get('Apps.mooc.controllers.CoursePutController');
-  router.put('/courses/:id', (req: Request, res: Response) => controller.run(req, res));
+  router.put('/courses/:id', reqSchema, validateReqSchema, (req: Request, res: Response) => controller.run(req, res));
 };
