e357ad8795c104ff0d736cdc0a2f920fc2b57632
Olek MP <olek.mp23@gmail.com>
2022-01-12 12:41:08 -0500

    ADD: MongoRepository.test.ts

--- tests/Contexts/Mooc/Courses/domain/CourseMother.ts
+++ tests/Contexts/Mooc/Courses/domain/CourseMother.ts
@@ -2,6 +2,9 @@ import { Course } from '../../../../../src/Contexts/Mooc/Courses/domain/Course';
 import { CourseDuration } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseDuration';
 import { CourseName } from '../../../../../src/Contexts/Mooc/Courses/domain/CourseName';
 import { CourseId } from '../../../../../src/Contexts/Mooc/Shared/domain/Courses/CourseId';
+import { CourseIdMother } from '../../Shared/domain/CourseIdMother';
+import { CourseDurationMother } from './CourseDurationMother';
+import { CourseNameMother } from './CourseNameMother';
 
 export class CourseMother {
   static create(id: CourseId, name: CourseName, duration: CourseDuration): Course {
@@ -10,5 +13,9 @@ export class CourseMother {
 
   static fromRequests({ id, name, duration }: { id: string; name: string; duration: string }) {
     return this.create(new CourseId(id), new CourseName(name), new CourseDuration(duration));
+  }
+
+  static random(): Course {
+    return this.create(CourseIdMother.random(), CourseNameMother.random(), CourseDurationMother.random());
   }
 }
